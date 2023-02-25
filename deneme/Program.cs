67c2a3a1700f0a96f824d07b292d39d8d67e631c
umitguner2002 <umitguner2002@hotmail.com>
2023-02-25 15:05:03 +0300

    üç sayının toplamı

--- deneme/Program.cs
+++ deneme/Program.cs
@@ -1,6 +1,7 @@
-﻿// iki sayının toplamı
+﻿// üç sayının toplamı
 int a = 5;
 int b = 3;
+int c = 10
 
 
-Console.WriteLine("Toplam: " + (a+b)); // sonuç
+Console.WriteLine("Toplam: " + (a+b+c)); // sonuç
